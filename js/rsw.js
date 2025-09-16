6ff3291777ce8c211ffcf217e342e03687a7ba12
quantummail <quantummail77@gmail.com>
2025-09-16 19:12:05 +0530

    Update rsw.js

--- js/rsw.js
+++ js/rsw.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator){
-  navigator.serviceWorker.register('/sw.js')
+  navigator.serviceWorker.register('/Florescence-v-2.0/sw.js')
     .then(reg => console.log('service worker registered'))
     .catch(err => console.log('service worker not registered', err));
 }
